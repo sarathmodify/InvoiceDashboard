6fedda5538a8751ce9cb77b27f80eceb6e764d2c
sarathmodify <sarathmanikam369@email.com>
2025-10-03 20:03:57 +0530

    feat: streaming implement in dashboard cards and latestinvoices

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,7 +4,9 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
+import {fetchCardData } from '../../lib/data';
 import { lusitana } from '@/app/ui/fonts';
+import { Invoice } from '@/app/lib/definitions';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,19 +15,22 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+ const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData();
+
+
 export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -40,7 +45,6 @@ export function Card({
   type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
-
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
